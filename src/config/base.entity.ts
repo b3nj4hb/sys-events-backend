17f1a6e5c3dcdbd07737a52f7584ad0db87d0e7b
b3nj4hb <benjalive12@gmail.com>
2024-08-28 17:09:35 -0500

    feat: Define base entity

---
+++ src/config/base.entity.ts
@@ -0,0 +1,24 @@
+import { STATES } from '../constants/states'
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm'
+
+export abstract class BaseEntity {
+	@PrimaryGeneratedColumn('uuid')
+	id: string
+	@CreateDateColumn({
+		type: 'timestamp',
+		name: 'created_at',
+	})
+	created_at: Date
+	@CreateDateColumn({
+		type: 'timestamp',
+		name: 'updated_at',
+	})
+	updated_at: Date
+	@Column({
+		type: 'enum',
+		enum: STATES,
+		default: STATES.Active,
+		name: 'state',
+	})
+	state!: STATES
+}
