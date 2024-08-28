c9a07aa66a6144717bd7f2cc8cc86ef4445af242
b3nj4hb <benjalive12@gmail.com>
2024-08-27 23:13:32 -0500

    feat: Add Event, Profile, and Student modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/database.config';
+import { ProfileModule } from './modules/profile/profile.module';
+import { StudentModule } from './modules/student/student.module';
+import { EventModule } from './modules/event/event.module';
+import { StudentEventModule } from './modules/student-event/student-event.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig),],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ProfileModule, StudentModule, EventModule, StudentEventModule,],
   controllers: [AppController],
   providers: [AppService],
 })
