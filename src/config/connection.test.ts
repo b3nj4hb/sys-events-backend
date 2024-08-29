1ae343e43117a84b184a98bee10c229d5ef682dc
b3nj4hb <benjalive12@gmail.com>
2024-08-28 20:21:03 -0500

    feat: Remove database connection testing

--- src/config/connection.test.ts
+++ src/config/connection.test.ts
@@ -4,26 +4,26 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export async function testConnection() {
-  const config = {
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: true,
-        ca: process.env.DB_CA,
-    },
-};
-  const client = new pg.Client(config);
-  await client.connect();
+	const config = {
+		user: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		database: process.env.DB_NAME,
+		ssl: {
+			rejectUnauthorized: true,
+			ca: process.env.DB_CA,
+		},
+	};
+	const client = new pg.Client(config);
+	await client.connect();
 
-  try {
-      const result = await client.query("SELECT VERSION()");
-      console.log(result.rows[0].version);
-  } catch (err) {
-      throw err;
-  } finally {
-      await client.end();
-  }
+	try {
+		const result = await client.query('SELECT VERSION()');
+		console.log(result.rows[0].version);
+	} catch (err) {
+		throw err;
+	} finally {
+		await client.end();
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { testConnection } from './config/connection.test';
+// import { testConnection } from './config/connection.test';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  await testConnection();
+	const app = await NestFactory.create(AppModule);
+	await app.listen(3000);
+	// await testConnection();
 }
 bootstrap();
