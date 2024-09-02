f299eb5c98b0aa1716bc2e50e9da787b81483f0f
b3nj4hb <benjalive12@gmail.com>
2024-09-02 16:32:48 -0500

    feat: Enable CORS for API endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS } from './constants';
 // import { testConnection } from './config/connection.test';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors(CORS);
 	await app.listen(3000);
 	// await testConnection();
 }
