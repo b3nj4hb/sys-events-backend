c9b4a34ed7f2a0d721aa61bf97aca2f860cebaba
b3nj4hb <benjalive12@gmail.com>
2024-09-02 20:29:27 -0500

    feat: Add Student Event service

--- src/modules/student-event/student-event.module.ts
+++ src/modules/student-event/student-event.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { StudentEventController } from './controllers/student-event.controller';
+import { StudentEventService } from './services/student-event.service';
 
-@Module({})
+@Module({
+  controllers: [StudentEventController],
+  providers: [StudentEventService]
+})
 export class StudentEventModule {}
