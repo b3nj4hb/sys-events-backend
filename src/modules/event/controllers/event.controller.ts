80dff46a3cbd7e39e6828ff1e4596c7620ec7b59
b3nj4hb <benjalive12@gmail.com>
2024-09-05 14:13:46 -0500

    Fix: Consistent naming for event endpoints

--- src/modules/event/controllers/event.controller.ts
+++ src/modules/event/controllers/event.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post,Put, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body } from '@nestjs/common';
 import { EventService } from '../services/event.service';
 import { CreateEventDto } from '../dto/create-event.dto';
 import { EventEntity } from '../entities/event.entity';
@@ -12,12 +12,12 @@ export class EventController {
 	async getEventsWithStudents() {
 		return this.eventService.getEventsWithStudents();
 	}
-  @Post('create-student')
-  async create(@Body() createEventDto: CreateEventDto): Promise<EventEntity> {
-    return this.eventService.createEvent(createEventDto);
-  }
-  @Put('update-student')
-  async update(@Body() updateEventDto: UpdateEventDto): Promise<EventEntity> {
-    return this.eventService.updateEvent(updateEventDto);
-  }
+	@Post('create-event')
+	async create(@Body() createEventDto: CreateEventDto): Promise<EventEntity> {
+		return this.eventService.createEvent(createEventDto);
+	}
+	@Put('update-event')
+	async update(@Body() updateEventDto: UpdateEventDto): Promise<EventEntity> {
+		return this.eventService.updateEvent(updateEventDto);
+	}
 }
