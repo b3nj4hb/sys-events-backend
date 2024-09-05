0db127bae66d6199a594751439e24681ddcdc538
b3nj4hb <benjalive12@gmail.com>
2024-09-05 14:23:41 -0500

    Fix: Remove unnecessary event type fields

--- src/modules/event/dto/create-event.dto.ts
+++ src/modules/event/dto/create-event.dto.ts
@@ -2,31 +2,31 @@
 import { IsString, IsNotEmpty, IsDateString, IsUUID } from 'class-validator';
 
 export class CreateEventDto {
-  @IsNotEmpty()
-  @IsString()
-  name: string;
+	@IsNotEmpty()
+	@IsString()
+	name: string;
 
-  @IsNotEmpty()
-  @IsDateString()
-  date: string;
+	@IsNotEmpty()
+	@IsDateString()
+	date: string;
 
-  @IsNotEmpty()
-  @IsString()
-  hour: string;
+	@IsNotEmpty()
+	@IsString()
+	hour: string;
 
-  @IsNotEmpty()
-  @IsString()
-  location: string;
+	@IsNotEmpty()
+	@IsString()
+	location: string;
 
-  @IsNotEmpty()
-  @IsString()
-  period: string;
+	@IsNotEmpty()
+	@IsString()
+	period: string;
 
-  @IsNotEmpty()
-  @IsString()
-  eventTypeName: string;
+	// @IsNotEmpty()
+	// @IsString()
+	// eventTypeName: string;
 
-  @IsNotEmpty()
-  @IsUUID()
-  eventTypeId: string; // ID del tipo de evento
+	@IsNotEmpty()
+	@IsUUID()
+	eventTypeId: string; // ID del tipo de evento
 }
