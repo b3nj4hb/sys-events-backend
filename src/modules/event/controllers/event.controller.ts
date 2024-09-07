010a159cc90f376ac176405bd08e2c03723bfa2b
b3nj4hb <benjalive12@gmail.com>
2024-09-07 16:04:06 -0500

    Refactor: Simplify event DTOs

--- src/modules/event/controllers/event.controller.ts
+++ src/modules/event/controllers/event.controller.ts
@@ -1,9 +1,8 @@
 import { Controller, Get, Post, Put, Body, UseInterceptors, UploadedFile, Delete, Param } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { EventService } from '../services/event.service';
-import { CreateEventDto } from '../dto/create-event.dto';
 import { EventEntity } from '../entities/event.entity';
-import { UpdateEventDto } from '../dto/update-event.dto';
+import { EventDto } from '../dto/event.dto';
 
 @Controller('event')
 export class EventController {
@@ -15,14 +14,12 @@ export class EventController {
 	}
 	@Post('create-event')
 	@UseInterceptors(FileInterceptor('file'))
-	async create(@Body() createEventDto: CreateEventDto, @UploadedFile() file: Express.Multer.File): Promise<EventEntity> {
-		// Si el archivo está presente, incluirlo en el DTO
-		createEventDto.file = file || undefined;
-		return this.eventService.createEvent(createEventDto);
+	async create(@Body() createEventDto: EventDto, @UploadedFile() file: Express.Multer.File): Promise<EventEntity> {
+		return this.eventService.createEvent({ ...createEventDto, file });
 	}
 	@Put(':id')
 	@UseInterceptors(FileInterceptor('file'))
-	async update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto, @UploadedFile() file: Express.Multer.File): Promise<EventEntity> {
+	async update(@Param('id') id: string, @Body() updateEventDto: EventDto, @UploadedFile() file: Express.Multer.File): Promise<EventEntity> {
 		return this.eventService.updateEvent({ ...updateEventDto, id, file });
 	}
 	@Delete('file/:id')
