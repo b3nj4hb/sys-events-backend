68996de7a2ffa3efa4017f9a71926f26026c78a8
b3nj4hb <benjalive12@gmail.com>
2024-09-07 17:09:24 -0500

    feat: Add Swagger documentation for student endpoints

--- src/modules/student/controllers/student.controller.ts
+++ src/modules/student/controllers/student.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { StudentService } from '../services/student.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('student')
 export class StudentController {
 	constructor(private readonly studentService: StudentService) {}
 
 	@Get('with-events')
+	@ApiOperation({ summary: "Retrieve student's with the events attendance status" })
 	async getStudentsWithEvents() {
 		return this.studentService.getStudentsWithEvents();
 	}
