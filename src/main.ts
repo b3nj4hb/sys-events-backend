b16326fbca33ceb757b895fcf9c819f8620b1aaa
David Rey <neisserrey@upeu.edu.pe>
2024-09-08 19:24:58 +0000

    fix: :ambulance: Validacion y classSerializerInterceptor
    * ValidationPipe para habilitar validacion en los DTO's
    * ClassSerializerInterceptor para poder excluir los campos al obtener las Entitys

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CORS } from './constants';
 import { SwaggerModule } from '@nestjs/swagger';
 import { SwaggerConfig } from './config/swagger.config';
+import { useContainer } from 'class-validator';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 // import { testConnection } from './config/connection.test';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors(CORS);
 
+	// Usar el ValidationPipe globalmente para validar DTOs
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+	// Habilitar el uso de ClassSerializerInterceptor para transformar las respuestas
+	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+	// Aquí le indicamos a class-validator que use el contenedor de NestJS
+	useContainer(app.select(AppModule), { fallbackOnErrors: true });
 	// swagger
 	const document = SwaggerModule.createDocument(app, SwaggerConfig);
 	SwaggerModule.setup('', app, document);
