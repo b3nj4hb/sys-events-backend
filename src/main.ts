fd94719ad3407c4ee0bc993c018fbf0638bd4aac
b3nj4hb <benjalive12@gmail.com>
2024-09-16 21:30:22 -0500

    Refactor: Add static file serving for public directory

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,20 @@ import { CORS } from './constants';
 import { SwaggerModule } from '@nestjs/swagger';
 import { SwaggerConfig } from './config/swagger.config';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import * as express from 'express';
 // import { testConnection } from './config/connection.test';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors(CORS);
 
+	// Serving static files
+	app.use('/public', express.static(join(__dirname, '..', 'public')));
+
 	// swagger
 	const document = SwaggerModule.createDocument(app, SwaggerConfig);
-	SwaggerModule.setup('', app, document);
+	SwaggerModule.setup('', app, document, { customCssUrl: '/public/swagger-dark.css' });
 
 	// dto validations
 	app.useGlobalPipes(new ValidationPipe());
