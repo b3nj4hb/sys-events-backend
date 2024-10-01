01fe9cc9efc1bf5ac6fecd4d865daeb0b0691783
b3nj4hb <benjalive12@gmail.com>
2024-10-01 07:36:27 -0500

    Refactor: Update database configuration to use environment variables

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,17 +3,19 @@ import * as dotenv from 'dotenv';
 
 dotenv.config(); // Carga las variables de entorno del archivo .env
 
-const isDropSchema = process.env.SEED_ENV === 'true';
+const isDropSchema = process.env.DB_DROP === 'true';
+const isSynchronize = process.env.DB_SYNC === 'true';
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
 	type: process.env.DB_TYPE as any,
-	host: process.env.DB_HOST,
+	host: isProduction ? process.env.DB_HOST_CLOUD : process.env.DB_HOST_LOCAL,
 	port: parseInt(process.env.DB_PORT, 10),
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 	entities: [__dirname + '/../modules/**/entities/*.entity{.ts,.js}'],
 	dropSchema: isDropSchema,
-	synchronize: isDropSchema,
-	ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true, ca: process.env.DB_CA } : undefined,
+	synchronize: isSynchronize,
+	ssl: isProduction ? { rejectUnauthorized: true, ca: process.env.DB_CA } : undefined,
 };
