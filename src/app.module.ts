d1eea7d16f6d512f0a2d6902f0f32f0f30d2cb88
b3nj4hb <benjalive12@gmail.com>
2024-10-09 17:57:25 -0500

    Refactor: Remove SeedModule and SeedService

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,20 +8,10 @@ import { StudentModule } from './modules/student/student.module';
 import { EventModule } from './modules/event/event.module';
 import { StudentEventModule } from './modules/student-event/student-event.module';
 import { CarrierModule } from './modules/carrier/carrier.module';
-import { SeedModule } from './seed/seed.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-	imports: [
-		TypeOrmModule.forRoot(typeOrmConfig),
-		ProfileModule,
-		StudentModule,
-		EventModule,
-		StudentEventModule,
-		CarrierModule,
-		SeedModule,
-		AuthModule,
-	],
+	imports: [TypeOrmModule.forRoot(typeOrmConfig), ProfileModule, StudentModule, EventModule, StudentEventModule, CarrierModule, AuthModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
